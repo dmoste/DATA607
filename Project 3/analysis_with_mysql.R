36ffc1cf6f0dd9b28c986416e46fa9cb4afdf40f
dmoste <60351405+dmoste@users.noreply.github.com>
2020-03-21 18:39:19 -0400

    Update analysis_with_mysql.R

--- Project 3/analysis_with_mysql.R
+++ Project 3/analysis_with_mysql.R
@@ -10,7 +10,7 @@ indeed1 <- dbGetQuery(con, sql)
 sql <- "SELECT * FROM reddit"
 reddit <- dbGetQuery(con, sql)
 
-data <- indeed1 %>%
+indeed_data <- indeed1 %>%
   filter(count > 100) %>%
   filter(token != "hiring") %>%
   filter(token != "jobs") %>%
@@ -36,10 +36,44 @@ data <- indeed1 %>%
   filter(token != "lab") %>%
   filter(token != "working")
                
-data$averages <- (data$count)/sum(data$count)
+indeed_data$averages <- (indeed_data$count)/sum(indeed_data$count)
 
-ggplot(data, aes(reorder(token, averages), averages)) +
+ggplot(indeed_data, aes(reorder(token, averages), averages)) +
  geom_bar(stat = "identity") +
  coord_flip() +
  labs(x = "Word",
       y = "Count")
+
+reddit_data <- reddit %>%
+  filter(count > 500) %>%
+  filter(token != "hiring") %>%
+  filter(token != "jobs") %>%
+  filter(token != "jobs") %>%
+  filter(token != "advice") %>%
+  filter(token != "skills") %>%
+  filter(token != "help") %>%
+  filter(token != "companies") %>%
+  filter(token != "find") %>%
+  filter(token != "salaries") %>%
+  filter(token != "events") %>%
+  filter(token != "indeed") %>%
+  filter(token != "career") %>%
+  filter(token != "employer") %>%
+  filter(token != "countries") %>%
+  filter(token != "years") %>%
+  filter(token != "ability") %>%
+  filter(token != "strong") %>%
+  filter(token != "sell") %>%
+  filter(token != "required") %>%
+  filter(token != "using") %>%
+  filter(token != "etc") %>%
+  filter(token != "lab") %>%
+  filter(token != "working")
+
+reddit_data$averages <- (reddit_data$count)/sum(reddit_data$count)
+
+ggplot(reddit_data, aes(reorder(token, averages), averages)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  labs(x = "Word",
+       y = "Count")
